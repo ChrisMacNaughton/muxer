f9946aadd9adad62b1e4a8b27a11136ae946cc5f
Chris MacNaughton <chmacnaughton@gmail.com>
2015-04-07 09:47:31 -0400

    tweak and add another test

--- spec/muxer/multiplexer_spec.rb
+++ spec/muxer/multiplexer_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe Muxer::Multiplexer do
   it 'lets a request wait on the longer one' do
     VCR.use_cassette('muxer/multiplexer/with_one_timeout') do
       multiplexer.add_url('https://github.com/', 0.0001)
-      multiplexer.add_url('https://codeclimate.com/', 5)
+      multiplexer.add_url('https://codeclimate.com/', 2)
       response = multiplexer.execute
 
       expect(response[:succeeded].count).to eq(2)
@@ -32,6 +32,18 @@ RSpec.describe Muxer::Multiplexer do
 
       expect(response[:succeeded].count).to eq(0)
       expect(response[:failed].count).to eq(1)
-   end
+    end
+  end
+
+  it 'kills one of two requests with a global timeout' do
+    VCR.use_cassette('muxer/multiplexer/with_global_timeout') do
+      multiplexer.add_url('https://github.com/')
+      multiplexer.add_url('https://www.google.com/')
+      multiplexer.timeout = 0.2
+      response = multiplexer.execute
+
+      expect(response[:succeeded].count).to eq(1)
+      expect(response[:failed].count).to eq(1)
+    end
   end
 end
